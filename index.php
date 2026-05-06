274b2d8a499b1bb867442f2ff2025607c8f5db5e
Jakub Uhrin <jakub.uhr279@gmail.com>
2026-05-06 12:59:10 +0200

    Priprava index.php

--- index.php
+++ index.php
@@ -1,17 +1,16 @@
-<?php include "databaza.php"; ?>
-
+</html>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Evidencia hier</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Evidencia Hier</title>
 </head>
 <body>
+    <h1>Evidencia hier</h1>
 
-<h1>Evidencia hier</h1>
+    <a href="add.php">Pridať hru</a>
 
-<a href="add.php">Pridať hru</a>
-
-<p>Zoznam hier sa zobrazí tu...</p>
-
+    <p>Zoznam hier sa zobrazí tu...</p>
 </body>
 </html>
